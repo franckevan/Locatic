1b4af774185bcb4c5e94892fe72b8538501f7b3f
Ouattara Dongui <ouattaradongui123@gmail.com>
2026-06-22 04:36:51 +0200

    feat: ajout de l'entité Brand et de l'interface du dépôt

---
+++ Locatic/Locatic.Domain/Entities/Brand.cs
@@ -0,0 +1,14 @@
+namespace Locatic.Domain.Entities
+{
+
+    public class Brand
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty ;
+
+        public string Country { get; set; } = string.Empty;
+
+        public ICollection<CarModel> Models { get; set; } = new List<CarModel>();
+    }
+}
---
+++ Locatic/Locatic.Domain/Interfaces/IBrandRepository.cs
@@ -0,0 +1,21 @@
+using Locatic.Domain.Entities;
+
+namespace Locatic.Domain.Interfaces{
+
+    public interface IBrandRepository
+    {
+        Task<IEnumerable<Brand>> GetAllAsync();
+
+
+        Task<Brand?> GetByIdAsync(int id);
+
+        Task AddAsync(Brand brand);
+
+        Task UpdateAsync(Brand brand);
+
+        Task DeleteAsync(int id);
+    }
+
+
+
+}
