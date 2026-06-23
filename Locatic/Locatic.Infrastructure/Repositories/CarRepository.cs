64159ad6876d5b4a3ab3a23f3f8be9f86209d0c6
Diarra <diarrabenm@gmail.com>
2026-06-23 22:55:32 +0200

    implement car management (DTOs, service, repo, controller)

---
+++ Locatic/Locatic.Infrastructure/Repositories/CarRepository.cs
@@ -0,0 +1,47 @@
+using Locatic.Domain.Entities;
+using Locatic.Domain.Interfaces;
+using Locatic.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+namespace Locatic.Infrastructure.Repositories{
+    public class CarRepository : ICarRepository
+    {
+        private readonly LocaticDbContext _context;
+        public CarRepository(LocaticDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Car>> GetAllAsync()
+        {
+            return await _context.Cars
+                .Include(c => c.CarModel)
+                .ThenInclude(m => m.Brand)
+                .ToListAsync();
+        }
+        public async Task<Car?> GetByIdAsync(int id)
+        {
+            return await _context.Cars
+                .Include(c => c.CarModel)
+                .ThenInclude(m => m.Brand)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+        public async Task AddAsync(Car car)
+        {
+            await _context.Cars.AddAsync(car);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Car car)
+        {
+            _context.Cars.Update(car);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var car = await _context.Cars.FindAsync(id);
+            if (car != null)
+            {
+                _context.Cars.Remove(car);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
