100070dea79a4049c1fc4d274722d8e649fd1dbb
Ouattara Dongui <ouattaradongui123@gmail.com>
2026-06-27 03:16:49 +0200

    Add ICarModelService interface

---
+++ Locatic/Locatic.Application/Interfaces/ICarModelService.cs
@@ -0,0 +1,20 @@
+using Locatic.Application.DTOs;
+
+namespace Locatic.Application.Interfaces{
+
+    public interface ICarModelService
+    {
+        Task<IEnumerable<CarModelDto>> GetAllAsync();
+
+        Task<CarModelDto?> GetByIdAsync(int id);
+
+        Task CreateAsync(CreateCarModelDto dto);
+
+        Task UpdateAsync(int id, CreateCarModelDto dto);
+
+        
+        Task DeleteAsync(int id);
+    }
+
+
+}
