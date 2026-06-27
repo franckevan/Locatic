cf33d81494a1765c3e91a030e5b0f5464c93561d
Sabinos Sabinos <franckevan2019@gmail.com>
2026-06-27 20:44:00 +0200

    add Db_context

---
+++ Locatic/Locatic.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,26 @@
+using Locatic.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Locatic.Infrastructure.Data{
+
+    public class LocaticDbContext : DbContext
+    {
+        public LocaticDbContext(DbContextOptions<LocaticDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; } 
+
+        public DbSet<CarModel> CarModels { get; set; }
+
+        public DbSet<Car> Cars { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        public DbSet<Reservation> Reservations { get; set; }
+
+    }
+
+}
---
+++ Locatic/Locatic.Infrastructure/Data/LocaticDbContextFactory.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Locatic.Infrastructure.Data{
+
+    public class LocaticDbContextFactory : IDesignTimeDbContextFactory<LocaticDbContext>
+    {
+        public LocaticDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<LocaticDbContext>();
+
+            optionsBuilder.UseSqlite("Data Source=locatic.db");
+
+            return new LocaticDbContext(optionsBuilder.Options);
+        }
+    }
+
+
+}
